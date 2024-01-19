11c9e8b498efbdfa476ad88ea95c3e11da9e647c
Fandi02 <fandimaulana936@gmail.com>
2024-01-20 00:04:53 +0700

    initial-project

---
+++ Assessment.Praktek.2.MVC/Assessment.Praktek.2.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Assessment.Praktek._2.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
